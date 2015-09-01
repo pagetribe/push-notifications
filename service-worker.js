9fe20b8b5c598bec0af0dbc73907e66078270ac4
Mark <pagetribemail@gmail.com>
2015-09-02 08:21:55 +1000

    Update service-worker.js
    add sendSubscriptionToServer function

--- service-worker.js
+++ service-worker.js
@@ -10,10 +10,26 @@ if ('serviceWorker' in navigator) {
       //TODO: Send the subscription subscription.endpoint
       //to your server and save it to tsend a push message and a later date
 
-      //return sendSubscriptionToServer(subscription);
+      return sendSubscriptionToServer(subscription);
     });
   }).catch(function(err) {
     // registration failed
     console.log('ServiceWorker registration failed: ', err);
   });
+}
+
+function sendSubscriptionToServer(subscription) {
+  // TODO: Send the subscription.endpoint
+  // to your server and save it to send a
+  // push message at a later date
+  //
+  // For compatibly of Chrome 43, get the endpoint via
+  // endpointWorkaround(subscription)
+  console.log('TODO: Implement sendSubscriptionToServer()');
+
+  // var mergedEndpoint = endpointWorkaround(subscription);
+
+  // This is just for demo purposes / an easy to test by
+  // generating the appropriate cURL command
+  // showCurlCommand(mergedEndpoint);
 }
