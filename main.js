a9db6c232575a26af50dc57ef748fb4b45ff83c2
Mark <pagetribemail@gmail.com>
2015-09-02 09:03:02 +1000

    changed files to main and service-worker

---
+++ main.js
@@ -0,0 +1,36 @@
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('service-worker.js').then(function(registration) {
+    //Registration was successfull
+    console.log("ServiceWorker registration successful with scope: ", registration.scope);
+    registration.pushManager.subscribe({userVisibleOnly: true})
+      .then(function(subscription) {
+        isPushEnabled = true;
+        //console.log("subscription.subcriptionId: ", subscription.subscriptionId);
+        console.log("subscription.endpoint: ", subscription.endpoint);
+      
+        //TODO: Send the subscription subscription.endpoint
+        //to your server and save it to tsend a push message and a later date
+
+        return sendSubscriptionToServer(subscription);
+    });
+  }).catch(function(err) {
+    // registration failed
+    console.log('ServiceWorker registration failed: ', err);
+  });
+}
+
+function sendSubscriptionToServer(subscription) {
+  // TODO: Send the subscription.endpoint
+  // to your server and save it to send a
+  // push message at a later date
+  //
+  // For compatibly of Chrome 43, get the endpoint via
+  // endpointWorkaround(subscription)
+  console.log('TODO: Implement sendSubscriptionToServer()');
+
+  // var mergedEndpoint = endpointWorkaround(subscription);
+
+  // This is just for demo purposes / an easy to test by
+  // generating the appropriate cURL command
+  // showCurlCommand(mergedEndpoint);
+}
--- service-worker.js
+++ service-worker.js
@@ -1,36 +1,41 @@
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('service-worker.js').then(function(registration) {
-    //Registration was successfull
-    console.log("ServiceWorker registration successful with scope: ", registration.scope);
-    registration.pushManager.subscribe({userVisibleOnly: true})
-      .then(function(subscription) {
-        isPushEnabled = true;
-        //console.log("subscription.subcriptionId: ", subscription.subscriptionId);
-        console.log("subscription.endpoint: ", subscription.endpoint);
-      
-        //TODO: Send the subscription subscription.endpoint
-        //to your server and save it to tsend a push message and a later date
+'use strict';
 
-        return sendSubscriptionToServer(subscription);
-    });
-  }).catch(function(err) {
-    // registration failed
-    console.log('ServiceWorker registration failed: ', err);
-  });
-}
+self.addEventListener('push', function(event) {
+  console.log('Received a push message', event);
 
-function sendSubscriptionToServer(subscription) {
-  // TODO: Send the subscription.endpoint
-  // to your server and save it to send a
-  // push message at a later date
-  //
-  // For compatibly of Chrome 43, get the endpoint via
-  // endpointWorkaround(subscription)
-  console.log('TODO: Implement sendSubscriptionToServer()');
+  var title = 'Yay a message.';
+  var body = 'We have received a push message.';
+  var icon = '/images/icon-192x192.png';
+  var tag = 'simple-push-demo-notification-tag';
 
-  // var mergedEndpoint = endpointWorkaround(subscription);
+  event.waitUntil(
+    self.registration.showNotification(title, {
+      body: body,
+      icon: icon,
+      tag: tag
+    })
+  );
+});
 
-  // This is just for demo purposes / an easy to test by
-  // generating the appropriate cURL command
-  // showCurlCommand(mergedEndpoint);
-}
+
+self.addEventListener('notificationclick', function(event) {
+  console.log('On notification click: ', event.notification.tag);
+  // Android doesn’t close the notification when you click on it
+  // See: http://crbug.com/463146
+  event.notification.close();
+
+  // This looks to see if the current is already open and
+  // focuses if it is
+  event.waitUntil(clients.matchAll({
+    type: "window"
+  }).then(function(clientList) {
+    for (var i = 0; i < clientList.length; i++) {
+      var client = clientList[i];
+      if (client.url == '/' && 'focus' in client)
+        return client.focus();
+    }
+    if (clients.openWindow)
+      return clients.openWindow('/');
+  }));
+
+});
